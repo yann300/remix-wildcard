b188ecc62e429c33cc440f9399215b7f713da06b
yann300 <yann.levreau@gmail.com>
2020-04-11 14:12:06 +0200

    fix res ipfs_plugin

--- src/hosts/ipfs-plugin.ts
+++ src/hosts/ipfs-plugin.ts
@@ -1,11 +1,12 @@
 import connect from 'connect';
+import request from 'request';
 
 export const ipfsPlugin = () => {
     const app = connect()
     app.use((req: any, res: any, next: any) => {
         console.log(req)
         const path = 'https://ipfsgw.komputing.org/ipfs/QmQmK435v4io3cp6N9aWQHYmgLxpUejjC1RmZCbqL7MJaM/' + req.path
-        req(path).pipe(res)
+        request(path).pipe(res)
     })
     return app
 }
