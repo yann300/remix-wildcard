cfeac8f18601f4bb68017d8579c0b7e318d949ae
yann300 <yann.levreau@gmail.com>
2020-04-12 10:40:03 +0200

    https on ipfsgw

--- src/hosts/ipfs-plugin.ts
+++ src/hosts/ipfs-plugin.ts
@@ -3,7 +3,7 @@ var proxy = require('express-http-proxy');
 
 export const ipfsPlugin = () => {
     const app = connect()
-    app.use('/ipfs', proxy('ipfsgw.komputing.org', {
+    app.use('/ipfs', proxy('https://ipfsgw.komputing.org', {
         proxyReqPathResolver: (req: any) => {
             return new Promise((resolve, reject) => resolve('/ipfs' + req.url));
           }
