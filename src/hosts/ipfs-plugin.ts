fd8cbcf8a58a0075df1705c7eda5fe6223125820
yann300 <yann.levreau@gmail.com>
2020-04-14 11:37:04 +0200

    allow iframe

--- src/hosts/ipfs-plugin.ts
+++ src/hosts/ipfs-plugin.ts
@@ -3,6 +3,10 @@ var proxy = require('express-http-proxy');
 
 export const ipfsPlugin = () => {
     const app = connect()
+    app.use('/ipfs', function (req: any, res: any, next: any) {
+        res.set('x-frame-options', '')
+        next();
+      });
     app.use('/ipfs', proxy('https://ipfsgw.komputing.org', {
         proxyReqPathResolver: (req: any) => {
             return new Promise((resolve, reject) => resolve('/ipfs' + req.url));
