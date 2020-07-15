b82197b833dbd57468533dd9dcd5f77ca4e1ce1a
yann300 <yann.levreau@gmail.com>
2020-07-15 10:14:24 +0200

    update embed

--- src/hosts/embedly.ts
+++ src/hosts/embedly.ts
@@ -6,6 +6,14 @@ export const embedly = () => {
     // this handled the certbot certificate verification for *all* the sub domains
     app.use('/.well-known', serveStatic('public/.well-known') as connect.HandleFunction)
     app.use('/', (req: any, res: any, next: any) => {
+        let baseUrl = 'https://remix.ethereum.org?'
+        const gist = req.query.gist
+        if (gist) baseUrl = `${baseUrl}minimizeterminal=true&gist=${gist}`
+        else baseUrl = `${baseUrl}&embed=true`
+
+        baseUrl = `${baseUrl}&plugins=solidity,udapp`
+        if (gist) baseUrl = `${baseUrl},fileExplorer`
+        
         res.setHeader('Content-Type', 'application/json');
         res.end(JSON.stringify({
             "version": "1.0",
@@ -17,7 +25,7 @@ export const embedly = () => {
             "author_name": "Remix team",
             "author_url": "https://remix-project.org",
         
-            "html": "<iframe src=\"https://remix.ethereum.org\" width=\"700\" height=\"825\" scrolling=\"yes\" frameborder=\"0\" allowfullscreen></iframe>",
+            "html": "<iframe src=\"" + baseUrl + "\" width=\"700\" height=\"825\" scrolling=\"yes\" frameborder=\"0\" allowfullscreen></iframe>",
             "width": 700,
             "height": 825,
         
