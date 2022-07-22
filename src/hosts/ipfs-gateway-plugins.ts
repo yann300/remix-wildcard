d15d97d2bfd4aa62a51f5fdcb02a023fca56fdb9
Filip Mertens <filip.mertens@ethereum.org>
2022-07-22 14:14:46 +0200

    fixes ipfs

--- src/hosts/ipfs-gateway-plugins.ts
+++ src/hosts/ipfs-gateway-plugins.ts
@@ -1,9 +1,22 @@
 import connect from 'connect';
+var proxy = require('express-http-proxy');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 export const ipfsGatewayPlugin = () => {
-    const app = connect()
-    app.use('/ipfs', createProxyMiddleware({ target: 'https://remix-project.mypinata.cloud/', changeOrigin: true }));
-    app.use('/', createProxyMiddleware({ target: 'https://ipfs-api.ethdevops.io/', changeOrigin: true }));
+    const app = connect() 
+    const projectId = '2A3pvVBZZTnGN2mAVnnq8gsOVRc'
+    const projectSecret = 'ac36044e46b771ba4b550e7a3bb56ef6'
+    const auth = 'Basic ' + Buffer.from(projectId + ':' + projectSecret).toString('base64')
+    app.use('/ipfs', proxy('https://ipfs2.ethdevops.io', {
+        proxyReqPathResolver: (req: any) => {
+            return new Promise((resolve, reject) => resolve('/ipfs' + req.url));
+          }
+    }))
+    // app.use('/ipfs', createProxyMiddleware({ target: 'https://remix-project.infura-ipfs.io', changeOrigin: true }));
+    // app.use('/api/v0/add', createProxyMiddleware({ target: 'https://ipfs.infura.io:5001', changeOrigin: true,
+   //     headers: {
+    //        Authorization: auth
+    //    } 
+    //}));
     return app
 }
