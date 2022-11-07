0d27dc79cd1e6e21f402bb906042dd1bf5cf553f
Filip Mertens <filip.mertens@ethereum.org>
2022-11-07 20:35:27 +0100

    ipfs cluster

--- src/hosts/ipfs-gateway-plugins.ts
+++ src/hosts/ipfs-gateway-plugins.ts
@@ -7,14 +7,15 @@ export const ipfsGatewayPlugin = () => {
     const projectId = '2A3pvVBZZTnGN2mAVnnq8gsOVRc'
     const projectSecret = 'ac36044e46b771ba4b550e7a3bb56ef6'
     const auth = 'Basic ' + Buffer.from(projectId + ':' + projectSecret).toString('base64')
-    /*
-    app.use('/ipfs', proxy('https://ipfs2.ethdevops.io', {
+    
+    app.use('/ipfs', proxy('https://ipfs-cluster.ethdevops.io', {
         proxyReqPathResolver: (req: any) => {
+		console.log(req.url)
             return new Promise((resolve, reject) => resolve('/ipfs' + req.url));
           }
     }))
-    */
-    app.use('/ipfs', createProxyMiddleware({ target: 'https://infura-ipfs.io', changeOrigin: true }));
+    
+   // app.use('/ipfs', createProxyMiddleware({ target: 'https://remix-project.mypinata.cloud', changeOrigin: true }));
     // app.use('/api/v0/add', createProxyMiddleware({ target: 'https://ipfs.infura.io:5001', changeOrigin: true,
    //     headers: {
     //        Authorization: auth
--- src/hosts/ipfs-plugin.ts
+++ src/hosts/ipfs-plugin.ts
@@ -1,16 +1,27 @@
 import connect from 'connect';
 var proxy = require('express-http-proxy');
 
+var requestAmount = 0
+
+
 export const ipfsPlugin = () => {
     const app = connect()
+    var orgtime = 0
+    setInterval(() => {
+        orgtime += 60
+        const average = requestAmount / orgtime
+        console.log(average, 'request / min')
+    }, 60000)
     app.use('/ipfs', function (req: any, res: any, next: any) {
+        console.log(req.url)
+        requestAmount++
         res.set('x-frame-options', '')
         next();
-      });
-    app.use('/ipfs', proxy('https://remix-project.mypinata.cloud', {
+    });
+    app.use('/ipfs', proxy('https://ipfs-cluster.ethdevops.io', {
         proxyReqPathResolver: (req: any) => {
             return new Promise((resolve, reject) => resolve('/ipfs' + req.url));
-          }
+        }
     }))
     return app
 }
