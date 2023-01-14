016e48c78a211c7ebb31e35d4134df8ffdd52de1
Filip Mertens <filip.mertens@ethereum.org>
2023-01-14 10:11:16 +0100

    rm data

--- src/hosts/ipfs-plugin.ts
+++ src/hosts/ipfs-plugin.ts
@@ -4,9 +4,6 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 export const ipfsPlugin = () => {
     const app = connect() 
-    const projectId = '2A3pvVBZZTnGN2mAVnnq8gsOVRc'
-    const projectSecret = 'ac36044e46b771ba4b550e7a3bb56ef6'
-    const auth = 'Basic ' + Buffer.from(projectId + ':' + projectSecret).toString('base64')
     
     app.use('/ipfs', proxy('https://ipfs-cluster.ethdevops.io', {
         proxyReqPathResolver: (req: any) => {
