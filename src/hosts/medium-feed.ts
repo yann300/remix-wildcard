e08a74ae8839db281faa50a9d68e8e848aa74372
yann300 <yann.levreau@gmail.com>
2023-06-20 17:58:31 +0200

    Delete medium-feed.ts

--- src/hosts/medium-feed.ts
+++
@@ -1,8 +0,0 @@
-import connect from 'connect';
-const { createProxyMiddleware } = require('http-proxy-middleware');
-
-export const ipfsGatewayPlugin = () => {
-    const app = connect()
-    app.use('/', createProxyMiddleware({ target: 'https://medium.com/feed/remix-ide', changeOrigin: true }));
-    return app
-}
